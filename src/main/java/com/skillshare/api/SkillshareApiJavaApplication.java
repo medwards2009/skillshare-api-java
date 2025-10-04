c5aa33d775b313f01c2a127092a21cbd737df334
Michael Edwards <michaeledwards2009@gmail.com>
2025-10-03 21:31:14 -0400

    init

---
+++ src/main/java/com/skillshare/api/SkillshareApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.skillshare.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillshareApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillshareApiJavaApplication.class, args);
+	}
+
+}
